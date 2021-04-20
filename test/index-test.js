9a94e93270619d6066332ba9a7f325a0f4c7a591
Rick <dirklo@gmail.com>
2021-04-20 12:00:11 +0200

    commit tree 846281d39141fb6a1f4ff1463839a461a398129d

---
+++ test/index-test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import React from 'react';
+
+describe('redux', () => {
+  it('updates the store on successful dispatch', () => {
+    expect(["1", "2"]).to.include("1")
+  })
+})
